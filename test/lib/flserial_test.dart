66fca156d030ff9c90205011c4aa97086a378619
grzesl <grzes_l@vp.pl>
2024-12-01 14:16:33 +0100

    Workflow tests

--- test/lib/flserial_test.dart
+++ test/lib/flserial_test.dart
@@ -1,12 +1,35 @@
 import 'dart:ffi';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:test/test.dart';
 import 'package:flserial/flserial.dart';
 
 void main() {
-  DynamicLibrary.open('libflserial.so');
+  const String libName = 'flserial';
+  String libPath = ''; 
+
+  /// The dynamic library in which the symbols for [FlserialBindings] can be found.
+  final DynamicLibrary dylib = () {
+    if (Platform.isMacOS || Platform.isIOS) {
+      return DynamicLibrary.open('$libPath.$libName.framework/$libName');
+    }
+    if (Platform.isAndroid || Platform.isLinux) {
+      libPath = '../build/linux/x64/release/shared/'; 
+      return DynamicLibrary.open('$libPath.lib$libName.so');
+    }
+    if (Platform.isWindows) {
+      return DynamicLibrary.open('$libPath.$libName.dll');
+    }
+    throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
+  }();
   //'/home/runner/work/flserial/flserial/build/linux/x64/release/shared/libflserial.so');
 
   test('FLSerial status should be closed', () {
+
+    if (kDebugMode) {
+      print(dylib.toString());
+    }
+
     final port = FlSerial();
 
     port.init();
