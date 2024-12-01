c062903f56d52dc55c6c084ded17d25a137da30c
grzesl <grzes_l@vp.pl>
2024-12-01 13:41:08 +0100

    List folder

--- test/lib/flserial_test.dart
+++ test/lib/flserial_test.dart
@@ -5,7 +5,7 @@ import 'package:test/test.dart';
 import 'package:flserial/flserial.dart';
 
 void main() {
-  DynamicLibrary.open('flserial.so');
+  DynamicLibrary.open('/home/runner/work/flserial/build/linux/x64/release/shared/lib/flserial.so');
 
   test('FLSerial status should be closed', () {
     final port = FlSerial();
