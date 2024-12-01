0d03301f59ec52fde64815076bbc0cc1b9167dd8
grzesl <grzes_l@vp.pl>
2024-12-01 12:51:59 +0100

    Test load library

--- test/lib/flserial_test.dart
+++ test/lib/flserial_test.dart
@@ -1,25 +1,12 @@
 import 'dart:ffi';
-import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 import 'package:flserial/flserial.dart';
 
 void main() {
+  DynamicLibrary.open('flserial.dll');
 
-  if (kReleaseMode) {
-    // I'm on release mode, absolute linking
-    final String localLib = path.join('data',  'flutter_assets', 'assets', 'flserial.dll');
-    String pathToLib = path.join(Directory(Platform.resolvedExecutable).parent.path, localLib);
-    DynamicLibrary.open(pathToLib);
-  } else {
-    // I'm on debug mode, local linking
-    var path = Directory.current.path;
-    DynamicLibrary.open('$path/assets/flserial.dll');
-  }
-  
   test('FLSerial status should be closed', () {
     final port = FlSerial();
 
@@ -30,7 +17,5 @@ void main() {
     expect(status, FLOpenStatus.closed);
 
     port.free();
-
-
   });
 }
